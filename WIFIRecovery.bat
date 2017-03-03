4cc4ec6feb66f6a8f1b60285dc2ffdc9caf5dd3b
ins1gn1a <ins1gn1a@users.noreply.github.com>
2017-03-03 15:17:42 +0000

    Fixed issue where certain SSIDs/PSKs caused error

--- WIFIRecovery.bat
+++ WIFIRecovery.bat
@@ -1,25 +1,14 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal EnableDelayedExpansion
 
 :main
     title WiFi Password recovery
    
-    echo Harvesting all known passwords!!!!!!!!!!!!
-
+    echo Harvesting all known passwords
 
     :: List Profiles
+	
     call :get-profiles r
-
-    :: Get the password for the WIFI Profile
-    :main-next-profile
-        for /f "tokens=1* delims=," %%a in ("%r%") do (
-            call :get-profile-key "%%a" key
-            if "!key!" NEQ "" (
-                echo WiFi Name: [%%a] Password: [!key!]
-            )
-            set r=%%b
-        )
-        if "%r%" NEQ "" goto main-next-profile
 
     echo.
     pause
@@ -27,42 +16,28 @@ setlocal enabledelayedexpansion
     goto :eof
 
 
-:: Get the WiFi key of any given profile
-:get-profile-key <1=profile-name> <2=out-profile-key>
-    setlocal
-
-    set result=
-
-    FOR /F "usebackq tokens=2 delims=:" %%a in (
-        `netsh wlan show profile name^="%~1" key^=clear ^| findstr /C:"Key Content"`) DO (
-        set result=%%a
-        set result=!result:~1!
-    )
-    (
-        endlocal
-        set %2=%result%
-    )
-
-    goto :eof
-
 ::
 :: Get all network profiles (comma separated) into the result result-variable
 :get-profiles <1=result-variable>
     setlocal
 
-    set result=
-
-   
     FOR /F "usebackq tokens=2 delims=:" %%a in (
         `netsh wlan show profiles ^| findstr /C:"All User Profile"`) DO (
         set val=%%a
         set val=!val:~1!
-
-        set result=%!val!,!result!
+		
+		FOR /F "usebackq tokens=2 delims=':'" %%k in (
+				`netsh wlan show profile name^="!val!" key^=clear ^| findstr /C:"Key Content"`) do (
+		set keys=%%k
+	
+		echo WiFi Name: [!val!] Password: [!keys!]
+		)
     )
     (
         endlocal
-        set %1=%result:~0,-1%
+		echo "Test"
+        set %1="test"
     )
 
     goto :eof
+	
