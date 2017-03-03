d70b6994228ac0241c9fc09f5dfdda08e27556be
LordNem <lordn3mesis@hotmail.com>
2017-03-03 15:33:46 +0000

    Update WIFIRecovery.bat

--- WIFIRecovery.bat
+++ WIFIRecovery.bat
@@ -1,34 +1,20 @@
 @echo off
 setlocal EnableDelayedExpansion
-
 :main
     title WiFi Password recovery
-   
     echo Harvesting all known passwords
-
-    :: List Profiles
-	
-    call :get-profiles r
-
-    echo.
+    call :get-Wifi-profiles r
     pause
-
     goto :eof
-
-	
-
-::
-:: Get all network profiles (comma separated) into the result result-variable
-:get-profiles <1=result-variable>
+:get-Wifi-profiles <1=result-variable>
     setlocal
-
     FOR /F "usebackq tokens=2 delims=:" %%a in (
         `netsh wlan show profiles ^| findstr /C:"All User Profile"`) DO (
         set val=%%a
         set val=!val:~1!
-		
-		FOR /F "usebackq tokens=2 delims=':'" %%k in (
-				`netsh wlan show profile name^="!val!" key^=clear ^| findstr /C:"Key Content"`) do (
+	
+	FOR /F "usebackq tokens=2 delims=':'" %%k in (
+		`netsh wlan show profile name^="!val!" key^=clear ^| findstr /C:"Key Content"`) do (
 		set keys=%%k
 	
 		echo WiFi Name: [!val!] Password: [!keys:~1!]
@@ -36,8 +22,6 @@ setlocal EnableDelayedExpansion
     )
     (
         endlocal
-        
     )
-
     goto :eof
 	
